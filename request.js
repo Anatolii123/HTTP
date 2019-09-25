9d7baeabab4faa5d6da09afa9869363ca1fdce96
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-25 16:07:43 +0500

    Request3Commit

--- request.js
+++ request.js
@@ -1,20 +1,44 @@
-let url = 'https://swapi.co/api/people/2/';
-let xhr = new XMLHttpRequest();
-xhr.open('GET', url);
-xhr.send();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 3) {
-        alert(`Загрузка`);
-    }
-};
-xhr.onload = function() {
-    if (xhr.status != 200) {
-        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
-        return;
-    }
-    alert(`Готово, получено ${xhr.response.length} байт`);
-    var obj = JSON.parse(xhr.response);
-    for (let key in obj) {
-        alert(key + ' = ' + obj[key]);
-    }
-};
+tableObj = document.createElement('table');
+for (let i = 87; i > 1; i--) {
+    d = document.getElementById("mySelect");
+    option = document.createElement("option");
+    option.text = i.toString();
+    // url1 = 'https://swapi.co/api/people/' + i + '/';
+    // xhr1 = new XMLHttpRequest();
+    // xhr1.open('GET', url1);
+    // xhr1.send();
+    // obj1 = JSON.parse(xhr1.response);
+    // option.value = i.toString();
+    d.add(option, d[1]);
+}
+
+function fun1() {
+    options=document.getElementById('mySelect').options;
+    sel = document.getElementById("mySelect").selectedIndex;
+    url = 'https://swapi.co/api/people/' + options[sel].text + '/';
+    div = document.getElementById("items");
+    tableObj.style.top = '10%';
+    tableObj.style.left = '5%';
+
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+            return;
+        }
+        obj = JSON.parse(xhr.response);
+        tableHTML = "";
+        for (let key in obj) {
+            row = '<tr><td>' + key + '</td><td>' + obj[key] + '</td></tr>';
+            tableHTML += row;
+        }
+        tableObj.innerHTML = tableHTML;
+        document.body.appendChild(tableObj);
+    };
+}
+
+
+
+
