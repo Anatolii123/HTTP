88809d5c4a3bad424669cd8edbc1472434f148c1
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-25 10:43:34 +0500

    RequestCommit

---
+++ request.js
@@ -0,0 +1,18 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://swapi.co/api/people/1/');
+xhr.send();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 3) {
+        alert(`Загрузка`);
+    }
+};
+xhr.onload = function() {
+    if (xhr.status != 200) {
+        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        return;
+    }
+    alert(`Готово, получено ${xhr.response.length} байт`);
+    // var map = new Map(JSON.parse(xhr.response));
+    // map.get("name");
+    alert(xhr.response.slice(1,7));
+};
