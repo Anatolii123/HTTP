312055f2067a843d6a96b6208fc8ed4fcddf6b65
AKusakin <anatolii.kusakin1234@gmail.com>
2019-09-26 12:36:06 +0500

    Request6Commit

--- request.js
+++ request.js
@@ -1,32 +1,58 @@
-tableObj = document.createElement('table');
-url = 'https://swapi.co/api/people/?page=1';
-tableObj.id = "myTable";
-for (let i = 9; i > 1; i--) {
-    d = document.getElementById("mySelect");
-    option = document.createElement("option");
-    option.text = i.toString();
-    d.add(option, d[1]);
+var tableObj = document.createElement('table');
+var obj2 = [];
+loadList("https://swapi.co/api/people/?page=1");
+
+function loadList(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = function () {
+        if (xhr.readyState != 4) {
+            return;
+        }
+        if (xhr.status != 200) {
+            alert("Что-то пошло не так..." + "\n" + xhr.response.toString());
+            return;
+        }
+
+        var response = JSON.parse(xhr.response);
+        var results = response.results;
+        for (var idx = 0; idx < results.length; idx++) {
+            obj2.push(results[idx]);
+        }
+
+        if(response.next){
+            loadList(response.next);
+        } else {
+            makeDropdownList(obj2);
+        }
+    }
+}
+
+function makeDropdownList(list) {
+    var select = document.getElementById("mySelect");
+    var text = document.getElementById("text");
+    select.style.visibility = "visible";
+    text.style.visibility = "hidden";
+    for (let i = 0; i < list.length; i++) {
+        var option = document.createElement("option");
+        option.text = (i + 1).toString();
+        select.options.add(option);
+    }
 }
 
 function fun1() {
     options=document.getElementById('mySelect').options;
     sel = document.getElementById("mySelect").selectedIndex;
-    xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onload = function() {
-        obj = JSON.parse(xhr.response);
-        obj2 = obj["results"];
-        tableHTML = "";
-        for (let key in obj2[options[sel].text]) {
-            row = '<tr><td>' + key + '</td><td>' + obj2[options[sel].text][key] + '</td></tr>';
-            tableHTML += row;
-        }
-        tableObj.innerHTML = tableHTML;
-        tableObj.style.top = '10%';
-        tableObj.style.left = '5%';
-        document.body.appendChild(tableObj);
-    };
+    tableHTML = "";
+    for (let key in obj2[options[sel].text]) {
+        row = '<tr><td>' + key + '</td><td>' + obj2[options[sel].text][key] + '</td></tr>';
+        tableHTML += row;
+    }
+    tableObj.innerHTML = tableHTML;
+    tableObj.style.top = '10%';
+    tableObj.style.left = '5%';
+    document.body.appendChild(tableObj);
 }
 
 
